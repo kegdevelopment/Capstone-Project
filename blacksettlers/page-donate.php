b3a8be67dbc6db0085af01b352c8fd7494426a06
kvoteiza <vasquezo@ualberta.ca>
2023-04-11 14:47:17 -0600

    Edited Donate Page.

--- blacksettlers/page-donate.php
+++ blacksettlers/page-donate.php
@@ -13,10 +13,6 @@ get_header();
   <main id="primary" class="site-main">
     <div class="donate-container">
       <h2>Donate</h2>
-      <div class="donate-content">
-        <p>Contributions from donors like you make it possible for us to continue our mission of educating Canadians on the role black settlers played in pioneering the lands of Alberta and Saskatchewan.</p>
-        <p>Support our cause by donating today!</p>
-      </div>
       <div class="donate-form">
         <?php echo do_shortcode('[give_form id="394"]'); ?>
       </div>
