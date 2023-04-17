0b9a7e89dc604dd262bb886d7d96409a4c1bbcd5
elhanyusuf <elhanyusuf75@gmail.com>
2023-04-16 21:27:34 -0600

    Video interview page into post type

--- blacksettlers/page-albums.php
+++ blacksettlers/page-albums.php
@@ -21,7 +21,7 @@
                 <p>Learn about the black settlers, their families, and the communities they settled in. Browse through our stories and discover something new.</p>
                 <a href="https://blacksettlersb.web.dmitcapstone.ca/communities/">
                 <div class="go-communities">
-                    Got To Communities
+                    Go To Communities
                     <img src="https://blacksettlersb.web.dmitcapstone.ca/wp-content/uploads/2023/04/icons8-arrow-24.png" alt="">
                 </div>
                 </a>
