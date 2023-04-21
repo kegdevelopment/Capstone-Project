b0a16722d3aec1c162c95aa02575055bfdbbd326
kvoteiza <vasquezo@ualberta.ca>
2023-04-20 22:10:31 -0600

    Changes size of mobile nav svg

--- blacksettlers/header.php
+++ blacksettlers/header.php
@@ -58,7 +58,7 @@
 		<!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<svg  class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<svg  class="menu-toggle" aria-controls="primary-menu" aria-expanded="false" width="50px" height="50px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M4 18L20 18" stroke="#000000" stroke-width="2" stroke-linecap="round"/>
 			<path d="M4 12L20 12" stroke="#000000" stroke-width="2" stroke-linecap="round"/>
 			<path d="M4 6L20 6" stroke="#000000" stroke-width="2" stroke-linecap="round"/>
